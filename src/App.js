742c6a1a603fb01cdd914c8c27151a34db69da26
hanhoing <hhy6789@gmail.com>
2022-03-14 16:55:58 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-
+import React, { Component } from 'react';
 import './App.css';
-import HHY from './Hhy';
 
-function App() {
-  return (
-    <div className="App">
-      <HHY></HHY>
-    </div>
-  );
+import Main from 'Main/Main';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import HHY from 'HHY/Hhy';
+class App extends Component {
+  render() {
+    return (
+      <>
+        <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Main  />}></Route>
+      <Route path="/hhy" element={<HHY />}></Route>
+        </Routes>
+      </BrowserRouter>
+      </>
+    );
+  }
 }
 
 export default App;
+
